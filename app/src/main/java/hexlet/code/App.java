b987f79cba297a2dc177cd5ea8db46f033d60c65
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2023-05-17 15:12:47 +0300

    modify help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,9 +3,22 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable{
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(description = "path to first file")
+    private String filepath1;
+
+    @Parameters(description = "path to second file")
+    private String filepath2;
 
     @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", help = true)
     private boolean helpRequested;
@@ -20,10 +33,13 @@ public class App implements Runnable{
         } else if (versionRequested) {
             System.out.println("Version 1.0");
         } else {
-            // Perform the logic to compare configuration files and show the differences
-            System.out.println("Comparing configuration files...");
+//            System.out.println("Comparing configuration files...");
+//            System.out.println("Format: " + format);
+//            System.out.println("Filepath 1: " + filepath1);
+//            System.out.println("Filepath 2: " + filepath2);
         }
     }
+
     public static void main(String[] args) {
         CommandLine.run(new App(), args);
     }
