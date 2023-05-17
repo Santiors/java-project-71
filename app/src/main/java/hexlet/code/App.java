fa9653258bbd6a27f7145a2e95d7c7ce2d5ed848
Gennady Vrublevsky <g.vrublevsky@geoscan.aero>
2023-05-17 14:52:01 +0300

    add help to project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable{
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", help = true)
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        } else if (versionRequested) {
+            System.out.println("Version 1.0");
+        } else {
+            // Perform the logic to compare configuration files and show the differences
+            System.out.println("Comparing configuration files...");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandLine.run(new App(), args);
     }
 }
